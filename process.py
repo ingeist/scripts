a649f2369d42fd0e1a2a87060efcb1815a762c55
j <jenny.xie6@gmail.com>
2020-04-18 01:17:39 -0700

    Add production of waveform

--- process.py
+++ process.py
@@ -1,9 +1,11 @@
 from PIL import Image
+import wavio
+import numpy as np
 img = Image.open("puzzle18.png")
 
 
 width, height = img.size
-
+stream=np.array([])
 
 for columns in range(0,width):
     white = 0
@@ -14,4 +16,10 @@ for columns in range(0,width):
             white += 1
         else:
             black += 1
-    print(black-126)
+    #print(black-126)
+    stream = np.append(stream,black-126)
+#print(stream)
+## create wav file
+
+rate = 10000
+wavio.write("18.wav", stream,rate, sampwidth =3)
